513d1cf205ec207a5338824fc31296a6b5084cd9
Samuel <samuelfs96@gmail.com>
2023-10-06 13:31:17 -0400

    add automatic name to generated pdf

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const getName = (name) => {
+    const date = new Date();
+    return `#${Math.floor(date.getTime() / 1000)}-${name}-MAYOKA-${
+      date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
+    }-${date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()}`;
+  };
+
   return (
     <div className="container mx-auto max-sm:px-0 max-lg:px-[5%] max-xl:px-[10%] max-2xl:px-[20%] px-[15%]">
       <div className="bg-black h-screen flex flex-col justify-between">
@@ -29,7 +36,7 @@ function App() {
           <PDFDownloadLink
             className="font-sofiaSans rounded-sm text-lg text-center bg-[#0097B2] border-white border-2 text-white py-2 w-[66%]"
             document={<Catalog products={products} />}
-            fileName="catalogo-prueba.pdf"
+            fileName={`${getName("CATALOGO")}.pdf`}
           >
             {({ blob, url, loading, error }) =>
               loading ? "Cargando..." : "Descargar Catálogo"
@@ -38,7 +45,7 @@ function App() {
           <PDFDownloadLink
             className="font-sofiaSans rounded-sm text-lg text-center bg-white border-[#0097B2] border-2 text-[#0097B2] py-2 w-[66%]"
             document={<Catalog products={products} />}
-            fileName="catalogo-prueba.pdf"
+            fileName={`${getName("REPORTE")}.pdf`}
           >
             {({ blob, url, loading, error }) =>
               loading ? "Cargando..." : "Descargar Reporte"
