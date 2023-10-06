45d9b7fc538fe03dfd9085c35883d587efc0d8f9
Samuel <samuelfs96@gmail.com>
2023-10-06 13:43:51 -0400

    mini refact and refresh after download

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import { PDFDownloadLink } from "@react-pdf/renderer";
 import Catalog from "./components/reports/Catalog";
 import logo from "./img/logo-white.png";
 
+const refresh = (after) =>
+  setTimeout(() => {
+    window.location.reload();
+  }, after);
+
+const getName = (name) => {
+  const date = new Date();
+  return `#${Math.floor(date.getTime() / 1000)}-${name}-MAYOKA-${
+    date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
+  }-${date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()}`;
+};
+
 function App() {
   const [products, setProducts] = useState([]);
   const productsCollectionRef = collection(db, "products");
+
   useEffect(() => {
     const getProducts = async () => {
       const data = await getDocs(productsCollectionRef);
@@ -19,13 +32,6 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const getName = (name) => {
-    const date = new Date();
-    return `#${Math.floor(date.getTime() / 1000)}-${name}-MAYOKA-${
-      date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
-    }-${date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()}`;
-  };
-
   return (
     <div className="container mx-auto max-sm:px-0 max-lg:px-[5%] max-xl:px-[10%] max-2xl:px-[20%] px-[15%]">
       <div className="bg-black h-screen flex flex-col justify-between">
@@ -34,6 +40,7 @@ function App() {
         </div>
         <div className="mb-[5rem] flex flex-col gap-2 items-center">
           <PDFDownloadLink
+            onClick={() => refresh(1000)}
             className="font-sofiaSans rounded-sm text-lg text-center bg-[#0097B2] border-white border-2 text-white py-2 w-[66%]"
             document={<Catalog products={products} />}
             fileName={`${getName("CATALOGO")}.pdf`}
@@ -43,6 +50,7 @@ function App() {
             }
           </PDFDownloadLink>
           <PDFDownloadLink
+            onClick={() => refresh(1000)}
             className="font-sofiaSans rounded-sm text-lg text-center bg-white border-[#0097B2] border-2 text-[#0097B2] py-2 w-[66%]"
             document={<Catalog products={products} />}
             fileName={`${getName("REPORTE")}.pdf`}
