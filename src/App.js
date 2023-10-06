2b0df7043b088b8ee06720a747140ae1ba1ff7bf
Samuel <samuelfs96@gmail.com>
2023-10-06 11:32:04 -0400

    create app ui

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { db } from "./utils/config/firebase-config";
 import { collection, getDocs } from "@firebase/firestore/lite";
-import { PDFDownloadLink } from '@react-pdf/renderer';
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import Catalog from "./components/reports/Catalog";
+import logo from "./img/logo-white.png";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -15,16 +16,36 @@ function App() {
       setProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getProducts();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className="container">
-      <PDFDownloadLink document={<Catalog products={products} />} fileName="catalogo-prueba.pdf">
-      {({ blob, url, loading, error }) =>
-        loading ? 'Loading document...' : 'Download now!'
-      }
-    </PDFDownloadLink>
+    <div className="container mx-auto max-sm:px-0 max-lg:px-[5%] max-xl:px-[10%] max-2xl:px-[20%] px-[15%]">
+      <div className="bg-black h-screen flex flex-col justify-between">
+        <div className="flex justify-center items-center">
+          <img className="w-[250px]" src={logo} alt="logo" />
+        </div>
+        <div className="mb-[5rem] flex flex-col gap-2 items-center">
+          <PDFDownloadLink
+            className="font-sofiaSans rounded-sm text-lg text-center bg-[#0097B2] border-white border-2 text-white py-3 w-[66%]"
+            document={<Catalog products={products} />}
+            fileName="catalogo-prueba.pdf"
+          >
+            {({ blob, url, loading, error }) =>
+              loading ? "Cargando..." : "Descargar Catálogo"
+            }
+          </PDFDownloadLink>
+          <PDFDownloadLink
+            className="font-sofiaSans rounded-sm text-lg text-center bg-white border-[#0097B2] border-2 text-[#0097B2] py-3 w-[66%]"
+            document={<Catalog products={products} />}
+            fileName="catalogo-prueba.pdf"
+          >
+            {({ blob, url, loading, error }) =>
+              loading ? "Cargando..." : "Descargar Reporte"
+            }
+          </PDFDownloadLink>
+        </div>
+      </div>
     </div>
   );
 }
