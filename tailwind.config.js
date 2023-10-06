cfb4e09e948b8053663f18dab0c5c8d9d235c078
Samuel <samuelfs96@gmail.com>
2023-10-06 11:31:22 -0400

    add font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sofiaSans: ["SofiaSansCondensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
